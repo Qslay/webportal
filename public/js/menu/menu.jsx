bcbb7f1653978ae2b71cbbc96256eb65eac1aeec
Malith077 <malith.git@gmail.com>
2019-06-13 21:39:22 +1000

    remove react parts

--- public/js/menu/menu.jsx
+++ public/js/menu/menu.jsx
@@ -37,77 +37,7 @@ class Home extends React.Component {
   render() {
     return (
       <>
-        <NavBarComponent />
-        <Container>
-          <Row>
-            <Col xs={12} md={12} lg={12}>
-              <h3>Menu</h3>
-            </Col>
-          </Row>
-          <Row>
-            {/* Create Category Section */}
-            <Col xs={4}>
-              <Row>
-                <Col xs={9}>Menu</Col>
-                <Col xs={3}>
-                  <i class="material-icons">add</i>
-                </Col>
-              </Row>
-              <Card cardTitle={'New Menu1'} />
-              <br />
-              <Card cardTitle={'New Menu2'} />
-
-              <Row>
-                <br />
-                <Col xs={12}>
-                  <Button onClick={() => (this.toggle(), this.setHead('Menu'))}>Add Menu</Button>
-                </Col>
-              </Row>
-            </Col>
-
-            {/* Create Item section */}
-            <Col xs={4}>
-              <Row>
-                <Col xs={9}>Category</Col>
-                <Col xs={3}>
-                  <i class="material-icons">add</i>
-                </Col>
-              </Row>
-              <Card cardTitle={'New Item1'} />
-              <br />
-              <Card cardTitle={'New Item2'} />
-              <Row>
-                <br />
-                <Col xs={12}>
-                  <Button onClick={() => (this.toggle(), this.setHead('Category'))}>Add Item</Button>
-                </Col>
-              </Row>
-            </Col>
-
-
-            {/* Adding additional info section */}
-            <Col xs={4}>3</Col>
-          </Row>
-
-          <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-            <ModalHeader toggle={this.toggle}>New {this.state.modalHeader}</ModalHeader>
-            <ModalBody>
-              <Form>
-                <FormGroup>
-                  <Label for="Menu" sm={3}>Add Menu</Label>
-                  <Col sm={10}>
-                    <Input type="text" name="Menu" id="Menu" ></Input>
-                  </Col>
-                </FormGroup>
-              </Form>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="primary" onClick={this.toggle}>Create {this.state.modalHeader}</Button>{' '}
-              <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-            </ModalFooter>
-          </Modal>
-
-        </Container>
+        
       </>
     );
 
